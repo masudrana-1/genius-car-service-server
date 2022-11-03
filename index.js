a6d756d405e090f71128a72cc73018a771709e70
masudrana-1 <masudrana58855@gmail.com>
2022-11-03 20:21:35 +0600

    server created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middle wars 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Genius car server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Genius car server is running on ${port}`);
+})
